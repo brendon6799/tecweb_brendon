f6085932deec4f4426fc79ace4fd167a760e1824
brendon6799 <61213371+brendon6799@users.noreply.github.com>
2020-04-01 22:15:30 -0300

    Add files via upload

--- intro-js-master/intro-js-master/src/Exe1.js
+++ intro-js-master/intro-js-master/src/Exe1.js
@@ -57,4 +57,16 @@ function operacoesBasicas(operacao, numero1, numero2) {
  * @param {*} elemento2
  * @returns {string} mensagem com o resultado da comparação 
  */
-function comparadorBasico(elemento1, elemento2) { }
+function comparadorBasico(elemento1, elemento2) {
+    if (elemento1 === elemento2) {
+        mensagem = ('Elemento ' + elemento1 + ' ('+ typeof(elemento1)+')' + ' é estritamente igual ao elemento ' + elemento2 + ' ('+ typeof(elemento2)+')')
+    }
+
+    if (elemento1 != elemento2) {
+        mensagem = ('Elemento ' + elemento1 + ' ('+ typeof(elemento1)+')' + ' é diferente do elemento ' + elemento2 + ' ('+ typeof(elemento2)+')')
+    }
+
+
+
+    return mensagem
+}
